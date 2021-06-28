139916eb026c624c358a32c55ea1378512749120
Angel Cervera <293444+angelcervera@users.noreply.github.com>
2021-06-28 22:48:33 +0100

    Menu

--- lib/main.dart
+++ lib/main.dart
@@ -9,105 +9,122 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Generic Template',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+          primarySwatch: Colors.grey,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: Scaffold(
+        appBar: AppBar(
+          leadingWidth: 256,
+          // leading: SizedBox(
+          //   width: 256,
+          //   child: Placeholder(),
+          // ), // This remove the Drawer button
+          title: Padding(
+            padding: const EdgeInsets.only(left: 10,),
+            child: Image.asset(
+              'placeholder.png',
+              height: 35.0,
+              fit: BoxFit.cover,
+            ),
+          ),
+          // actions: [Placeholder(), Placeholder()],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 18.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: 256,
+                child: MainMenu(),
+              ),
+              Expanded(child: Body())
+            ],
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+class MenuGroup extends StatelessWidget {
+  const MenuGroup({Key? key, required this.groupTitle, required this.groupIcon})
+      : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final String groupTitle;
+  final IconData groupIcon;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      color: Colors.black12,
+      child: ListTile(
+        leading: Icon(groupIcon),
+        title: Text(groupTitle),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class MainMenu extends StatelessWidget {
+  const MainMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListView(
+        children: [
+          MenuGroup(
+              groupTitle: "NFT Management",
+              groupIcon: Icons.filter_frames_outlined),
+          ListTile(
+            leading: Icon(
+              Icons.report,
+            ),
+            title: Text("Report"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.manage_search,
+            ),
+            title: Text("Management"),
+            onTap: () {},
+          ),
+          Divider(),
+          MenuGroup(
+              groupTitle: "Administration",
+              groupIcon: Icons.admin_panel_settings),
+          ListTile(
+            leading: Icon(
+              Icons.person,
+            ),
+            title: Text("Users"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.lock,
+            ),
+            title: Text("Roles"),
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      ),
+    );
+  }
+}
+
+class DeviceType {}
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
   }
 }
